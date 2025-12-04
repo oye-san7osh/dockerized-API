b609f32f0f95c303bcd98597efaf230154c80c28
Santosh Paudel <santoshpaudel322@gmail.com>
2025-12-04 15:15:23 +0545

    chore(settings): add fvb_student, rest_framework, django_filters app to INSTALLED_APPS

--- app/API/settings.py
+++ app/API/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fvb_student',
+    'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
