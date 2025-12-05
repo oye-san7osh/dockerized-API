49665b5707ca7cfe49661f0a71fe58ceae1b0d0e
Santosh Paudel <santoshpaudel322@gmail.com>
2025-12-05 08:07:04 +0545

    feat(urls): add detail url to urls.py

--- app/fvb_student/urls.py
+++ app/fvb_student/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from fvb_student.views import StudentListCreate
+from fvb_student.views import StudentListCreate, StudentDetailUpdateDelete
 
 app_name = "FunctionBasedAPI"
 
 urlpatterns = [
     path('students/', StudentListCreate, name = 'student-show-list'),
+    path('students/<int:pk>/', StudentDetailUpdateDelete, name = 'student-detail-update-delete-int'),
+    #path('students/<slug:slug>/', StudentDetailUpdateDelete, name = 'student-detail-update-delete-text'),
+    
     
 ]
