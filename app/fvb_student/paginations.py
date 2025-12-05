1a0d8ff9f53e97444faf9454b9a8c389e48dc94b
Santosh Paudel <santoshpaudel322@gmail.com>
2025-12-05 20:44:45 +0545

    feat(pagination): add LimitOffsetPagination, CursorPagination to StudentPagination

--- app/fvb_student/paginations.py
+++ app/fvb_student/paginations.py
@@ -1,3 +1,6 @@
+
+# pagination by PageNumberPagination
+
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
@@ -39,4 +42,91 @@ class StudentPagination(PageNumberPagination):
                 "results": data
             }
         )
+
+
+
+
+"""
+# pagination by LimitOffsetPagination
+
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+
+
+class StudentPagination(LimitOffsetPagination):
     
+    default_limit = 4
+    limit_query_param = "limit"
+    offset_query_param = "offset"
+    max_limit = 5
+    
+    
+    def get_limit(self, request):
+        
+        if request.query_params.get("limit"):
+            return int(request.query_params.get("limit"))
+        return self.default_limit
+    
+    
+    def get_offset(self, request):
+        
+        if request.query_params.get("offset"):
+            return int(request.query_params.get("offset"))
+        return 0
+    
+    
+    def get_paginated_response(self, data):
+        
+        return Response(
+            {
+                "status": "success",
+                "page_info":
+                    {
+                        "limit": self.limit,
+                        "offset": self.offset,
+                        "total_items": self.count,
+                    },
+                "links":
+                    {
+                        "next_url": self.get_next_link(),
+                        "previous_url": self.get_previous_link(),
+                    },
+                "result": data
+            }
+        )
+    """ 
+    
+"""    
+# Pagination by CursorPagination
+
+
+from rest_framework.pagination import CursorPagination
+from rest_framework.response import Response
+
+
+class StudentPagination(CursorPagination):
+       
+       page_size = 4
+       cursor_query_param = 'cursor'
+       ordering = "-admission_date"
+       
+       
+       def get_paginated_response(self, data):
+           
+           return Response(
+               {
+                   "status": "success",
+                   "pagination":
+                       {
+                           "page_size": self.page_size,
+                           "ordering": self.ordering,
+                       },
+                    "links":
+                        {
+                            "next_url": self.get_next_link(),
+                            "previous_url": self.get_previous_link(),
+                        },
+                    "results": data
+               }
+           )
+"""
