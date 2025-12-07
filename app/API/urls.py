62835d8bc1f678ef065147aec9fa40cfce855726
Santosh Paudel <santoshpaudel322@gmail.com>
2025-12-07 08:50:34 +0545

    feat(urls): add jwt endpoints to urlpattern

--- app/API/urls.py
+++ app/API/urls.py
@@ -1,8 +1,18 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # JWT AUTHENTICATION ENDPOINTS
+    path('api/token/', TokenObtainPairView.as_view(), name = "token_obtain_par"),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name = "token_refresh"),
+    
+    
     path('api/', include("fvb_student.urls")),
 ]
