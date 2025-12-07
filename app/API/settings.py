6218cc7d08e084e67b3074a62fba47650f0f6e48
Santosh Paudel <santoshpaudel322@gmail.com>
2025-12-07 09:08:11 +0545

    chore(settings): update jwt setting

--- app/API/settings.py
+++ app/API/settings.py
@@ -127,7 +127,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES':(
-        'rest_framework_simplejwt,authentiation.JWTAuthentication',
-    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
 }
+
